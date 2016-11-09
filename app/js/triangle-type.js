888f7c3b0ebc1f30c0f947c3a817573ed3f7394c
Rafael Lages <rafael.pinto@corp.globo.com>
2016-11-08 22:59:05 -0200

    triangle type code and tests

--- app/js/triangle-type.js
+++ app/js/triangle-type.js
@@ -1,1 +1,65 @@
-
+(function (global) {
+  "use strict";
+  
+  global.Triangle = function Triangle(){};
+  
+  Triangle.prototype.checkType = function (a, b, c) {
+    var type = null;
+  
+    // first we need to validate if our parameters are numbers
+    if (!this.validateNumberParam(a, b, c)) {
+      console.error("Invalid parameters: All parameters should be a numbers -- given: ", arguments);
+    
+      // then check if its a equilateral
+    } else if (this.isEquilateral(a, b, c)) {
+      type = "equilateral";
+    
+      // or a isosceles
+    } else if (this.isIsosceles(a, b, c)) {
+      type = "isoceles";
+    
+      // if its a triangle but none above its a scalene
+      // so no need to create an isScalene function
+    } else if (this.isTriangle(a, b, c)) {
+      type = "scalene";
+    
+      // otherwise its not a triangle
+    } else {
+      console.error("It's not a Triangle: Given parameters don't form a triangle -- given: ", arguments);
+    }
+    return type;
+  };
+  
+  Triangle.prototype.validateNumberParam = function () {
+    var param, type, isValid = true;
+    // using arguments to make it work with one or more parameters
+    for (var i = 0; i < arguments.length; i++) {
+      param = arguments[i];
+      type = typeof param;
+      // number verification taken from jquery source
+      if ((type !== "number" && type !== "string") || isNaN(param - parseFloat(param))) {
+        isValid = false;
+        break;
+      }
+    }
+    return isValid;
+  };
+  
+  Triangle.prototype.isEquilateral = function (a, b, c) {
+    // verify if all 3 sides are identical
+    return (a === b && b === c);
+  };
+  
+  Triangle.prototype.isIsosceles = function (a, b, c) {
+    // verify if only 2 sides are identical
+    // its better to call isEquilateral again than duplicate its logic
+    return (a === b || b === c || c === a) && !this.isEquilateral(a, b, c);
+  };
+  
+  Triangle.prototype.isTriangle = function (a, b, c) {
+    // verify if it is actually a triangle
+    // it must have a side smaller than sum of others
+    return (a + b > c && a + c > b && b + c > a );
+  };
+  
+})(window);
--- test/triangle-type-Spec.js
+++ test/triangle-type-Spec.js
@@ -1,1 +1,79 @@
+describe("TriangleTypeSpec", function () {
+  var t = new Triangle();
+  
+  
+  describe("validateNumberParamSpec", function () {
+    it("should validade a single number", function () {
+      expect(t.validateNumberParam(8)).toBe(true);
+      expect(t.validateNumberParam("5")).toBe(true);
+    });
+    
+    it("should not validade an invalid string", function () {
+      expect(t.validateNumberParam("5 plz break")).toBe(false);
+    });
+    
+    it("should validade multiple numbers", function () {
+      expect(t.validateNumberParam(8, 11.3, "18", -5)).toBe(true);
+    });
+    
+    it("should not validate when an invalid parameter is present", function () {
+      expect(t.validateNumberParam(8, 10, {ok: 8}, 1)).toBe(false);
+    });
+  });
+  
+  describe("isEquilateralSpec", function () {
+    it("should return true when all sides are equal", function () {
+      expect(t.isEquilateral(8, 8, 8)).toBe(true);
+    });
+    
+    it("should return false when one or more sides are different", function () {
+      expect(t.isEquilateral(8, 4, 8)).toBe(false);
+      expect(t.isEquilateral(8, 8, 5)).toBe(false);
+    });
+  });
 
+  describe("isIsoscelesSpec", function () {
+    it("should return false when all sides are equal", function () {
+      expect(t.isIsosceles(8, 8, 8)).toBe(false);
+    });
+    
+    it("should return false when all sides are diferent", function () {
+      expect(t.isIsosceles(4, 5, 6)).toBe(false);
+    });
+    
+    it("should return true when only two sides are egual", function () {
+      expect(t.isIsosceles(8, 4, 8)).toBe(true);
+      expect(t.isIsosceles(8, 8, 5)).toBe(true);
+    });
+  });
+  
+  describe("isIsoscelesSpec", function () {
+    it("should return false when all sides are equal", function () {
+      expect(t.isIsosceles(8, 8, 8)).toBe(false);
+    });
+    
+    it("should return false when all sides are diferent", function () {
+      expect(t.isIsosceles(4, 5, 6)).toBe(false);
+    });
+    
+    it("should return true when only two sides are egual", function () {
+      expect(t.isIsosceles(8, 4, 8)).toBe(true);
+      expect(t.isIsosceles(8, 8, 5)).toBe(true);
+    });
+  });
+  
+  describe("isTriangleSpec", function () {
+    it("should return true if a side is smaller than sum of others", function () {
+      expect(t.isTriangle(8, 8, 8)).toBe(true);
+      expect(t.isTriangle(4, 8, 8)).toBe(true);
+      expect(t.isTriangle(8, 4, 8)).toBe(true);
+      expect(t.isTriangle(5, 4, 4)).toBe(true);
+    });
+    
+    it("should return false when sum of two sides smaller than other", function () {
+      expect(t.isTriangle(200, 5, 6)).toBe(false);
+      expect(t.isTriangle(5, 5, 12)).toBe(false);
+      expect(t.isTriangle(7, 20, 7)).toBe(false);
+    });
+  });
+});
